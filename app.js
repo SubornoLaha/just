8713f68c53faf9500c7eb810827bc13be51b00e0
SubornoLaha <sb.laha45@gmail.com>
2022-11-16 19:21:13 +0530

    just

---
+++ app.js
@@ -0,0 +1,28 @@
+const express=require("express")
+const bodyParser=require('body-parser')
+const { urlencoded } = require("body-parser")
+const app=express()
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+        res.sendFile(__dirname+"/index.html")
+})
+
+app.post("/",(req,res)=>{
+       const password=req.body.psswrd
+       if(password==11){
+        res.sendFile(__dirname+'/views/main.html')
+       }
+})
+
+app.post("/love",function(req,res){
+        res.send("Thank you")
+})
+
+app.post("/hate",(req,res)=>{
+        res.send("Thank you for your hate!")
+})
+
+app.listen(3000,()=>{
+        console.log("server is running at port 3000!")
+})
